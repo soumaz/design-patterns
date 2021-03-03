dca3fa67de5038f8a29074711a1c4d45bd495721
Souvik Mazumdar <soumaz07@gmail.com>
2021-03-03 20:18:04 +0530

    Added singleton pattern

---
+++ SingletonPattern/SingletonPattern/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                () => FirstCall(),
+                () => SecondCall()
+                );
+        }
+
+            private static void FirstCall()
+            {
+                Singleton ton = Singleton.GetInstance;
+                ton.Logging("I am doing logging");
+            }
+            
+            private static void SecondCall()
+            {
+                Singleton ton1 = Singleton.GetInstance;
+                ton1.Logging("I am doing logging again");
+            }
+           
+
+            //Singleton.Inheritance heritance = new Singleton.Inheritance();
+            //heritance.Logging("Calling Inherited class");
+
+        
+    }
+}
---
+++ SingletonPattern/SingletonPattern/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        //public static readonly Object obj = new object();
+        private static readonly Lazy<Singleton> instance = new Lazy<Singleton>(() => new Singleton());
+        public static Singleton GetInstance
+        {
+            get
+            {           
+                return instance.Value;
+            }
+        }
+
+        private Singleton()
+        {
+            counter = counter + 1;
+            Console.WriteLine(counter.ToString());
+        }
+
+        public void Logging(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+    }
+
+    
+}
